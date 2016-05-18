df81d074ef0da2604495375cc1a197f0aa6243f9
Frank Iriarte <jurrish@gmail.com>
2016-05-17 21:31:12 -0700

    still need to add eventlistener and such

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var allProducts = [];
-var productNames = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'sweep', 'unicorn', 'usb', 'water_can', 'wine_glass'];
+var productNames = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'unicorn', 'sweep', 'usb', 'water_can', 'wine_glass'];
 
 function Product(name, path) {//Object constructor
   // TODO: Build your constructor and necessary properties.
@@ -16,22 +16,59 @@ function Product(name, path) {//Object constructor
 })();
 
 var productRank = {
-  elImgOne: document.getElementById('image-one'),
-  elImgTwo: document.getElementById('image-two'),
-  elImgThree: document.getElementById('image-three'),
+  totalClicks: 0,
+  elObjOne: document.getElementById('image-one'),
+  elObjTwo: document.getElementById('image-two'),
+  elObjThree: document.getElementById('image-three'),
+  imageEls: document.getElementById('images'),
+  resultsEls: document.getElementById('results'),
+  showResultsEls: document.getElementById('showResults'),
+  resetEls: document.getElementById('reset'),
 
   getRandomIndex: function() {
     return Math.floor(Math.random() * productNames.length);
   },
 
   displayImages: function() {
-    productRank.elImgOne.src = allProducts[this.getRandomIndex()].path;
-    productRank.elImgTwo.src = allProducts[this.getRandomIndex()].path;
-    productRank.elImgThree.src = allProducts[this.getRandomIndex()].path;
-    if(productRank.elImgOne.src === productRank.elImgTwo.src || productRank.elImgOne.src === productRank.elImgThree.src || productRank.elImgTwo.src === productRank.elImgThree.src) {
+    productRank.elObjOne.src = allProducts[this.getRandomIndex()].path;
+    productRank.elObjTwo.src = allProducts[this.getRandomIndex()].path;
+    productRank.elObjThree.src = allProducts[this.getRandomIndex()].path;
+    if(productRank.elObjOne.src === productRank.elObjTwo.src || productRank.elObjOne.src === productRank.elObjThree.src || productRank.elObjTwo.src === productRank.elObjThree.src) {
       this.displayImages();
+    } else {
+      this.elObjOne.id = this.elObjOne.name;
+      this.elObjTwo.id = this.elObjTwo.name;
+      this.elObjThree.id = this.elObjThree.name;
     }
   },
+  //build event listener, and everytime it's clicked, increment the counter??
+  tallyClicks: function(elId) {
+    for (var i in allProducts) {
+      if (allProducts[i].name === elId) {
+        allProducts[i].tally += 1;
+        this.totalClicks += 1;
+        console.log(allProducts[i].name + ' has ' + allProducts[i].tally + ' votes');
+        console.log(this.totalClicks + ' : this.totalClicks');
+      }
+    }
+  },
+
+  displayResults: function() {
+    var ulEl = document.createElement('ul');
+    for(var i in allProducts) {
+      var liElOne = document.createElement('li');
+      var str = allProducts[i].name + ' has ' + allProducts[i].tally + ' votes';
+      liElOne.textContent = str.charAt(0).toUpperCase() + str.slice(1);
+      ulEl.appendChild(liElOne);
+    }
+    var liElTwo = document.createElement('li');
+    liElTwo.textContent = 'Total Clicks = ' + productRank.totalClicks;
+    ulEl.appendChild(liElTwo);
+    this.resultsEls.appendChild(ulEl);
+  }
+  // showButton: function() {
+  //
+  // }
 };
 //   // TODO: All the properties of the object! What do you think you need? Try to write one piece at a time and make sure it does what you want before writing a little more.
 //   // NOTE: A-C-P reminder... Make very intentional and iterative changes to your code, and then A-C-P.
